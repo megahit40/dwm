febf95fe6d481d4df91bb7cc9b7858ca7e234eab
Eilert Palm Vesterkjær <epv@online.no>
2022-06-08 11:15:25 +0200

    modified:   config.def.h

--- config.def.h
+++ config.def.h
@@ -1,40 +1,61 @@
 /* See LICENSE file for copyright and license details. */
 
+#include <X11/XF86keysym.h>
+
 /* appearance */
-static const char font[]            = "-*-*-medium-*-*-*-14-*-*-*-*-*-*-*";
-static const char normbordercolor[] = "#cccccc";
-static const char normbgcolor[]     = "#cccccc";
-static const char normfgcolor[]     = "#000000";
-static const char selbordercolor[]  = "#0066ff";
-static const char selbgcolor[]      = "#0066ff";
-static const char selfgcolor[]      = "#ffffff";
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const Bool showbar           = True;     /* False means no bar */
-static const Bool topbar            = True;     /* False means bottom bar */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int gappx     = 1;        /* gap pixel between windows */
+static const unsigned int snap      = 20;       /* snap pixel */
+static const int showbar            = 1;        /* 0 means no bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
+static const char *fonts[]          = { "monospace:size=10" };
+static const char dmenufont[]       = "monospace:size=10";
+static const char col_gray1[]       = "#222222";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray4[]       = "#eeeeee";
+static const char col_cyan[]        = "#005577";
+static const char *colors[][3]      = {
+	/*               fg         bg         border   */
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+};
+/*
+static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",    NULL };
+static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%",    NULL };
+static const char *mutevol[]  = { "/usr/bin/pactl", "set-sink-mute",   "0", "toggle", NULL }; 
+*/
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
+	/* xprop(1):
+	 *	WM_CLASS(STRING) = instance, class
+	 *	WM_NAME(STRING) = title
+	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            True,        -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       False,       -1 },
+	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
-static const float mfact      = 0.55; /* factor of master area size [0.05..0.95] */
-static const Bool resizehints = True; /* True means respect size hints in tiled resizals */
+static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const int nmaster     = 1;    /* number of clients in master area */
+static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ "|M|",      centeredmaster },
+	{ ">M>",      centeredfloatingmaster },
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -45,8 +66,9 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *dmenucmd[] = { "dmenu_run", "-fn", font, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "uxterm", NULL };
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -55,6 +77,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
@@ -63,6 +87,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[3]} },
+	{ MODKEY,                       XK_o,      setlayout,      {.v = &layouts[4]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
@@ -71,6 +97,9 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,			XK_minus,  setgaps,      			   {.i = -1 } },
+	{ MODKEY,			XK_plus,  setgaps,       			   {.i = +1 } },
+	{ MODKEY|ShiftMask,		XK_plus,  setgaps,      		   {.i = 0  } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -81,10 +110,34 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	
+	{ 0, 			XF86XK_AudioLowerVolume, 	 spawn,		SHCMD("amixer sset 'Master' 5%-; kill -36 $(pidof dwmblocks) ") },
+	{ 0, 			XF86XK_AudioMute,	 	 spawn,		SHCMD("amixer sset 'Master' toggle-mute; kill -36 $(pidof dwmblocks) ") },
+	{ 0, 			XF86XK_AudioRaiseVolume, 	 spawn,		SHCMD("amixer sset 'Master' 5%+; kill -36 $(pidof dwmblocks) ") },
+
+/*	{ 0, 			XF86XK_AudioMute,         	 spawn,		 {.v = mutevol } },
+	{ 0, 			XF86XK_AudioRaiseVolume,	 spawn,		 {.v = upvol } },
+	{ ShiftMask,		XF86XK_AudioPlay,  		 spawn,		SHCMD("mpc pause; kill -35 $(pidof dwmblocks); notify-send pause") },
+*/
+/*	{ 0, 			XF86XK_AudioPlay,       	 spawn,		SHCMD("mpc toggle; kill -35 $(pidof dwmblocks); notify-send -t 5000 play/pause") }, 
+	{ 0, 			XF86XK_AudioNext,	 	 spawn,		SHCMD("mpc next; kill -35 $(pidof dwmblocks); notify-send -t 5000 next") },
+	{ 0, 			XF86XK_AudioPrev,  	 	 spawn,		SHCMD("mpc prev; kill -35 $(pidof dwmblocks); notify-send -t 5000 prev") },
+	{ ShiftMask,		XF86XK_AudioNext,  		 spawn,		SHCMD("mpc seekthrough +10; kill -35 $(pidof dwmblocks); notify-send -t 5000 skip +10") },
+	{ ShiftMask,		XF86XK_AudioPrev,  		 spawn,		SHCMD("mpc seekthrough -10; kill -35 $(pidof dwmblocks); notify-send -t 5000 skip -10") },
+	{ MODKEY,		XF86XK_AudioNext,  	 	 spawn,		SHCMD("mpc seekthrough +60; kill -35 $(pidof dwmblocks); notify-send -t 5000 skip +60") },
+	{ MODKEY,		XF86XK_AudioPrev,	 	 spawn,		SHCMD("mpc seekthrough -60; kill -35 $(pidof dwmblocks); notify-send -t 5000 skip -60") },
+*/	{ MODKEY,			XK_e,	  		 spawn, 	SHCMD("st" " -e neomutt") },
+	{ MODKEY,			XK_w,	   		 spawn, 	SHCMD("brave-browser") },
+	{ MODKEY|ShiftMask,     	XK_l,			 spawn,         SHCMD("st" " -e slock") },
+	{ MODKEY|ShiftMask,     	XK_m,			 spawn,         SHCMD("st" " -e ncmpcpp") },
+	{ MODKEY|ShiftMask,     	XK_s,			 spawn,         SHCMD("slack") },
+	{ 0,    		XF86XK_Calculator,		 spawn,         SHCMD("xcalc") },
+	{ 0,			XK_Print,			 spawn,         SHCMD("xfce4-screenshooter") },
+	{ 0,		    	XF86XK_HomePage,		 spawn,         SHCMD("brave-browser" " startpage.com") },
 };
 
 /* button definitions */
-/* click can be ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
+/* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
 	/* click                event mask      button          function        argument */
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
